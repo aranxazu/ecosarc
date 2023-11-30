36043b4d353e8e521c6db7368609af914abdfd63
aranxazu <aranxazu.mg@gmail.com>
2023-11-29 23:01:48 -0300

    Se agregan los cambio en el archivo flask_app.py

--- Flask_app.py
+++ Flask_app.py
@@ -7,19 +7,22 @@ app = Flask(__name__)
 
 # Rutas
 @app.route('/')
-def index():
-    return render_template('index.html')
-
+def pagina_inicio():
+    return render_template('inicio.html')
+    
 @app.route('/fondos-mutuos')
 def fondos_mutuos():
     return render_template('index.html')
 
 @app.route('/rentabilidades', methods=['GET', 'POST'])
 def rentabilidades():
-    fecha_datepicker = request.form.get('datepicker')
+    
     df = pd.read_csv('uploads/rentabilidades_acumuladas.csv', sep=";", index_col=None)
-
     df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
+    
+
+    fecha_datepicker = request.form.get('datepicker')
+
     fechas = df['fecha'].unique()
 
     if fecha_datepicker is None:
@@ -45,6 +48,7 @@ def portafolios():
     df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
 
     fondos = df['Run Fondo'].unique()
+    nombres = df['Nombre Fondo Mutuo'].unique()
 
     run = str(request.form.get('fondos'))
     fecha = request.form.get("datepicker")
@@ -61,7 +65,7 @@ def portafolios():
     else:
         html = get_portafolio(run, tipo_cartera, fecha)
 
-    return render_template('portafolios.html', fondos=fondos, tipos=['Nacional', 'Internacional'], html=html)
+    return render_template('portafolios.html', fondos=fondos, tipos=['Nacional', 'Internacional'], html=html, nombres=nombres)
 
 @app.route('/detalle-fondo/<run>', methods=['POST','GET'])
 def detalle_fondo(run):
@@ -70,12 +74,30 @@ def detalle_fondo(run):
 
 # Funciones
 def get_rentabilidades(df):
+    def color(valor):
+        if valor > 0:
+            return 'green'  # Si es positivo, el color será verde
+        elif valor < 0:
+            return 'red'  # Si es negativo, el color será rojo
+        else:
+            return 'black'  # Si es cero, el color será negro
+
+    #Cambiar el formato y establecer el color a la columna
+    df['Rentabilidad'] = df['Rentabilidad'].apply(lambda x: f'<span style="color: {color(x)}">{x * 100:.2f}%</span>')
     df['Run Fondo'] = df["Run Fondo"].apply(crear_enlace)
+    #Cambiar presentación de periodo
+    df['Período'] = df['Período'].replace('1M', '1 mes')
+    df['Período'] = df['Período'].replace('3M', '3 meses')
+    df['Período'] = df['Período'].replace('6M', '6 meses')
+    df['Período'] = df['Período'].replace('1Y', '1 año')
+    df['Período'] = df['Período'].replace('YTD', 'Año hasta la fecha')
     html_table = df.to_html(classes='table', index=False, escape=False)
+    html_table = html_table.replace('<table', '<table style="width:100%;"')
     return html_table
 
 def get_detalle_fondo(run):
     df = pd.read_csv('uploads/detalle_fondo.csv', sep=";", index_col=None)
+    df.fillna('sin información', inplace=True)
     df['Run Fondo'] = df['Run Fondo'].astype(str)
     df = df[df['Run Fondo'] == run]
     html_table = df.to_html(classes='table', index=False, escape=False)
@@ -83,27 +105,51 @@ def get_detalle_fondo(run):
 
 def get_series(run):
     df = pd.read_csv('uploads/series.csv', sep=";", index_col=None)
+    df.fillna('sin información', inplace=True)
     df['Run Fondo'] = df['Run Fondo'].astype(str)
     df = df[df['Run Fondo'] == run]
+
+    df['Valor inicial cuota'] = df['Valor inicial cuota'].astype(int)
+
     html_table = df.to_html(classes='table', index=False, escape=False)
     return html_table
 
 def get_portafolio(run, tipo_cartera, fecha):
     df = pd.read_csv(f'uploads/portafolio_{tipo_cartera}.csv', sep=";", index_col=None)
+    df.fillna('sin información', inplace=True)
     df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
     df['Run Fondo'] = df['Run Fondo'].astype(str)
     df = df[df['Run Fondo'] == run]
     df = df[df['fecha'] == fecha]
     df['Run Fondo'] = df["Run Fondo"].apply(crear_enlace)
 
+
+    def color(valor):
+        if valor > 0:
+            return 'green'  # Si es positivo, el color será verde
+        elif valor < 0:
+            return 'red'  # Si es negativo, el color será rojo
+        else:
+            return 'black'  # Si es cero, el color será negro
+
+    porcentaje_cols = [
+        'Porcentaje del capital del emisor',
+        'Porcentaje del total de activos del emisor',
+        'Porcentaje del total de activos del fondo'
+    ]
+    for col in porcentaje_cols:
+        if col in df.columns:
+            df[col] = df[col].apply(lambda x: f'<span style="color: {color(x)}">{x * 100:.2f}%</span>')
+
     if df.empty:
         html_table = '<p>No hay información para el periodo seleccionado</p>'
     else:
         html_table = df.to_html(classes='form-table', index=False, escape=False)
+        html_table = html_table.replace('<table', '<table style="width:100%;"')
     return html_table
 
 def crear_enlace(run):
-    html = f'<a id="aunder" onclick="submitForm(\'{run}\')" name="{run}" value="{run}">{run}</a>'
+    html = f'<a id="aunder"  href="/detalle-fondo/{run}">{run}</a></a>'
     html += f'<input type="hidden" name="run" value="{run}">'
     return html
 
@@ -115,6 +161,7 @@ def ultimo_dia_del_mes(fecha):
     fecha_cierre_mes = datetime(fecha.year, fecha.month, ultimo_dia)
 
     return fecha_cierre_mes
+
 
 if __name__ == '__main__':
     app.run(debug=True)
